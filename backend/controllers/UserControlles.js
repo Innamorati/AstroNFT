752587bc5a462dcabc0964bb803a65b7c5ba7de7
Innamorati <73516778+Innamorati@users.noreply.github.com>
2022-04-05 11:13:49 -0300

    User signup

--- backend/controllers/UserControlles.js
+++ backend/controllers/UserControlles.js
@@ -2,29 +2,38 @@ const usuario = require('../models/UserModels')
 
 const UserControllers = {
 
-    // userRegistration: (req, res) => {
-    //     const { firstName, lastName, email, password, from, image } = req.body
-    //     new usuario({ firstName, lastName, email, password, from, image }).save()
-    //         .then((respuesta) => res.json({ respuesta }))
-    //     try {
-    //         res.json({
-    //             success: true,
-    //             from: "registro",
-    //             message: "Success true"
-    //         })
-    //     }
-    //     catch (err) {
-    //         console.log(err)
-    //         res.json({ succes: false, message: "Succes false" })
-    //     }
-    // }
-    userRegistration: (req, res) => {
-        const { firstName, lastName, email, password, from, image } = req.body
-        new usuario({ firstName, lastName, email, password, from, image }).save()
-            .then((respuesta) => res.json({ respuesta }))
+    userRegistration: async (req, res) => {
+        const { firstName, lastName, email, password, from, image } = req.body.data
 
-        // res.json({respuesta: productos})
-    },
+        try {
+            const userExist = await usuario.findOne({ email })
+            if (userExist) {
+                res.json({ succes: false, message: "User exist" })
+            }
+            else {
+                const newUSer = new usuario({
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    from,
+                    image,
+                    verifiedMail: false,
+                })
+                await newUSer.save()
+                res.json({
+                    success: true,
+                    from: "registro",
+                    message: "Success true"
+                })
+            }
+
+        }
+        catch (err) {
+            console.log(err)
+            res.json({ succes: false, message: "Succes false" })
+        }
+    }
 
 }
 

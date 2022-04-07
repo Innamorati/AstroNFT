327764e5c61190e6ac5a64e8228a8edaa52089f1
AliciaPak25 <aliciappak@gmail.com>
2022-04-07 10:37:51 -0300

    adding of a route

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />

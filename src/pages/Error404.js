87aaf6dee7e1198b2b5bca9a30cca2a72f49e67e
AliciaPak25 <aliciappak@gmail.com>
2022-04-08 01:06:13 -0300

    changes in error 404 styles file

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,13 +1,17 @@
 import React from "react";
-import {BackgroundError404, ButtonBackToHome} from '../styles/StyleError404';
+import {BackgroundError404, ButtonBackToHome, ImageError} from '../styles/StyleError404';
+import { Link as LinkRouter } from 'react-router-dom';
 
 const Error404 = () => {
     return(
         <BackgroundError404>
             <p>Error 404</p>
-            <ButtonBackToHome>
-                Back to Home
-            </ButtonBackToHome>
+            <p>Sorry! Page not found ☹</p>
+            <ImageError>
+            <img src={process.env.PUBLIC_URL + '/assets/error1.jpg'} alt="astronaut" />
+            {/* <img src={process.env.PUBLIC_URL + '/assets/error4.jpg'} alt="astronaut-moon" /> */}
+            </ImageError>
+            <LinkRouter to={'/home'}> <ButtonBackToHome>Back to Home</ButtonBackToHome> </LinkRouter>
         </BackgroundError404>
     );
 }
--- src/styles/StyleError404.js
+++ src/styles/StyleError404.js
@@ -6,14 +6,28 @@ export const BackgroundError404 = styled.div`
     display: flex;
     justify-content: center;
     flex-direction: column;
-    background-color: #83B1FF;
+    background-color: #97c6fe; /* #83B1FF; */
 
-    p:nth-child(1){
+    p:nth-child(1) {
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 15rem;
+    font-size: 9rem;
     font-weight: 700;
+    color: #333333;
+    }
+
+    p:nth-child(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+    font-size: 2rem;
+    color: #333333;
+    }
+
+    a {
+        text-decoration: none;
     }
 `;
 
@@ -27,16 +41,13 @@ export const ButtonBackToHome = styled.button`
     justify-content: center;
     border: none;
     margin: auto;
+    color: #333333;
+`;
 
-    button:nth-child(1){
-        background-color: #f2c94c;
-        border: none;
-        color: white;
-        font-weight: 700;
-        font-size: 1.1rem;
-        letter-spacing: 2px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+export const ImageError = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 10%;
+    width: 100%;
 `;

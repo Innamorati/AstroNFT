d787e443a2d6753f15d56a59d75ca23c3e4d05f6
kevindarnet.work@gmail.com <kevindarnet.work@gmail.com>
2022-04-08 22:10:38 -0300

    Buttons Products & responsive

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+
 import {
   Title,
   ItemProduct,
   PriceUser,
   UserName,
+  UserImg,
   Eth,
   ConteinerProduct,
   IconEth,
   DivPriceETH,
   ArsMadeBy,
   DivArs,
+  ViewMore,
+  AddCart,
+  DivButtons,
+  ConteinerUser,
 } from "../styles/StyledProducts";
 
 //ESTILOS DE PRODUCTS Y PRODUCT ESTAN EN UNICO COMPONENTE DE ESTILOS
@@ -40,12 +47,43 @@ const Product = () => {
             />
             <Eth>0.121 ETH</Eth>
           </DivPriceETH>
-          <UserName>GIIIO</UserName>
+          <ConteinerUser>
+            <UserImg
+              style={{
+                backgroundImage: `url('${
+                  process.env.PUBLIC_URL + "/assets/userImage.png"
+                }')`,
+                backgroundPosition: "center center",
+                objectFit: "contain",
+              }}
+            />
+            <UserName>GIIIO</UserName>
+          </ConteinerUser>
         </PriceUser>
         <DivArs>
           <ArsMadeBy>≈ ARS$ 46,828.55</ArsMadeBy>
           <ArsMadeBy>Made By</ArsMadeBy>
         </DivArs>
+        <DivButtons>
+          <LinkRouter to={"/details"}>
+            <ViewMore>View more</ViewMore>
+          </LinkRouter>
+          <AddCart>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path
+                fill="currentColor"
+                d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
+              ></path>
+            </svg>
+            <span>Add</span>
+          </AddCart>
+        </DivButtons>
       </ConteinerProduct>
     </>
   );
---
+++ src/pages/DetailsProducts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FooterComp from "../components/Footer";
+
+import { FatherDetails } from "../styles/StyleDetailsProducts";
+
+export default function Body() {
+  return (
+    <>
+      <FatherDetails></FatherDetails>
+      <FooterComp />
+    </>
+  );
+}

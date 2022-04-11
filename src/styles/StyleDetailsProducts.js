ac84e874201dd3395c75c0940d734a951e7af538
fernandogtorrez <fernandogtorrez@gmail.com>
2022-04-11 09:57:55 -0500

    edit

--- src/styles/StyleDetailsProducts.js
+++ src/styles/StyleDetailsProducts.js
@@ -154,115 +154,15 @@ export const BtnBuy = styled.button`
   font-weight: 600;
 `;
 
-export const CreatorDetails = styled.div``;
-
 export const Creator = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 1rem;
 `;
 
-export const TitleCreator = styled.div``;
 
-export const DivCreator = styled.div``;
 export const ConteinerTitleAndLike = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
->>>>>>> 2860d34bf86b7949108f2470ae70050f490874de
-`;
-
-export const ImgDetails = styled.div`
-  width: 370px;
-  height: 370px;
-  background-position: center;
-  background-size: cover;
-  border-radius: 10px;
-`;
-
-export const HeaderDetails = styled.div`
-  width: 100%;
-`;
-
-export const TitleDetails = styled.h1`
-  font-size: 2rem;
-`;
-
-export const CategoryDetails = styled.p`
-  font-size: 1rem;
-  background-color: gray;
-  padding: 1rem;
-`;
-
-export const BtnLike = styled.div`
-  width: 50px;
-  height: 50px;
-  background-color: aquamarine;
-`;
-
-export const HeaderDetails2 = styled.div`
-  /* display: flex;
-  justify-content: space-between; */
-  width: 100%;
-`;
-
-export const PriceFatherDetails = styled.div`
-  font-size: 2rem;
-`;
-
-export const PriceDetails = styled.div`
-  font-size: 2rem;
-`;
-
-export const TitlePrice = styled.h2`
-  font-size: 2rem;
-`;
-
-export const DivPrice = styled.div`
-
-`;
-
-export const IconEther = styled.div`
-  width: 20px;
-  height: 40px;
-  background-size: cover;
-  background-position: center;
-`;
-
-export const EtherDetails = styled.div`
-  font-size: 1.3rem;
-  font-weight: 500;
-`;
-
-export const ArMoney = styled.div`
-  font-size: 1rem;
-`;
-
-export const BtnDetails = styled.div`
-  background-color: yellowgreen;
-`;
-
-export const BtnBuy = styled.button`
-  background-color: blue;
-  padding: .5rem 1rem;
-  border: none;
-  font-size: 1rem;
-`;
-
-export const CreatorDetails = styled.div`
-  
-`;
-
-export const Creator = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 1rem;
-`;
-
-export const TitleCreator = styled.div`
-  color: green;
-`;
-
-export const DivCreator = styled.div`
-  background-color: bisque;
 `;

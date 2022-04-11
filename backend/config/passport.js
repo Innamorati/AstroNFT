18d0f9b1b6c5be1e85dd0337ce03589d3a5aaad8
Innamorati <73516778+Innamorati@users.noreply.github.com>
2022-04-11 10:47:11 -0300

    Merge branch 'Alicia' of https://github.com/Innamorati/AstroNFT into Andres

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,14 +1,15 @@
 const passport = require('passport')
 const jwtStrategy = require('passport-jwt').Strategy
 const extractJwt = require('passport-jwt').ExtractJwt
-
 const User = require('../models/UserModels')
 
 module.exports = passport.use(new jwtStrategy({
     jwtFromRequest: extractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.SECRET_KEY
 }, (jwt_payload, done) => {
+
     User.findOne({ _id: jwt_payload.id })
+
         .then(user => {
             if (user) {
                 return done(null, user)
@@ -21,7 +22,7 @@ module.exports = passport.use(new jwtStrategy({
             }
         })
         .catch(err => {
+            console.log(err.status)
             return done(err, false)
         })
-
 }))

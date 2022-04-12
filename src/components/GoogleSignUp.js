202be893dd5d71c6034e9ec4903836810b5d278d
Innamorati <73516778+Innamorati@users.noreply.github.com>
2022-04-12 09:50:32 -0300

    Google signin and signup

--- src/components/GoogleSignUp.js
+++ src/components/GoogleSignUp.js
@@ -1,47 +1,45 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login'
-/* import { connect } from 'react-redux'; */
-/* import userActions from '../../redux/actions/userActions'; */
+import { connect } from 'react-redux';
+import userActions from '../redux/actions/UserActions';
 import { GoogleButton } from "../styles/StyleSign";
 
 function GoogleSignUp(props) {
 
   const responseGoogle = (response) => {
-      console.log(response);
-      const googleUser = {
-        firstName: response.profileObj.givenName,
-        lastName: response.profileObj.familyName,
-        email: response.profileObj.email,
-        password: response.profileObj.googleId,
-        google: true,
-      }
+    console.log(response);
+    const data = {
+      firstName: response.profileObj.givenName,
+      lastName: response.profileObj.familyName,
+      email: response.profileObj.email,
+      password: response.profileObj.googleId,
+      from: "google",
+      image: response.profileObj.imageUrl,
     }
+    console.log(response)
+    props.userSignUp(data)
+  }
 
 
-
+  console.log(props)
   return (
     <GoogleButton>
-    <GoogleLogin
-      clientId="213097106218-go202vf7di8bbd7c7g9vkn685ptqdbeh.apps.googleusercontent.com"
-      buttonText="Sign Up with Google"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={'single_host_origin'}
-    />
+      <GoogleLogin
+        clientId="213097106218-go202vf7di8bbd7c7g9vkn685ptqdbeh.apps.googleusercontent.com"
+        buttonText="Sign Up with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </GoogleButton>
 
   );
 }
-export default GoogleSignUp;
-/* const mapStateToProps = (state) => {
-    return{
-        user: state.userReducer.user,
-    }
-}
+
 const mapDispatchToProps = {
-  signUpUser: userActions.signUpUser,
+  userSignUp: userActions.userSignUp,
 
 }
-export default connect(mapStateToProps, mapDispatchToProps)(GoogleSignUp); */
+export default connect(null, mapDispatchToProps)(GoogleSignUp);
 
 

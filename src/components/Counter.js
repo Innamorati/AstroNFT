706699b46a5a855011c336cd587024c375722553
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-04-14 02:15:49 -0300

    Countup agregado

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Counter, Count1, Count2, Count3, Count4 } from "../styles/CounterStyle"
+import CountUp from "react-countup";
 
 const HeroCounter = () => {
     return (
         <>
             <Counter>
                 <Count1>
-                    <p>259K</p>
+                <CountUp start={0} end={259} useEasing={true} delay={0.5} duration={4} suffix='k' style={{fontSize: '32px', fontFamily: 'ubuntu', fontWeight: '800'}}/>
                     <p>NFT Sold</p>
                     <p>Last Month</p>
                 </Count1>
                 <Count2>
-                    <p>10K</p>
+                <CountUp start={0} end={10} useEasing={true} delay={0.5} duration={4} suffix='k' style={{fontSize: '32px', fontFamily: 'ubuntu', fontWeight: '800'}}/>
                     <p>New User</p>
                     <p>Last Week</p>
                 </Count2>
                 <Count3>
-                    <p>200</p>
+                <CountUp start={0} end={200} useEasing={true} delay={0.5} duration={4} suffix='+' style={{fontSize: '32px', fontFamily: 'ubuntu', fontWeight: '800'}}/>
                     <p>New NFT</p>
                     <p>ON Market</p>
                 </Count3>
                 <Count4>
-                    <p>2</p>
+                <CountUp start={0} end={2.76} decimals={2} useEasing={true} delay={0.5} duration={4} suffix='ETH' style={{fontSize: '32px', fontFamily: 'ubuntu', fontWeight: '800'}}/>
                     <p>Floor Price</p>
                     <p>Last Month</p>
                 </Count4>

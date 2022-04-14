7c58c6bb588e390d951fd1f40c8ed547d0f06ab5
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-04-14 06:05:15 -0300

    cambios para deploy

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const app = express()
 require('dotenv').config();
 require('./config/database')
 
+const path = require('path');
+
 app.use(cors())
 app.use(express.json())
 app.use('/api', Router)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    })
+}
 
-app.listen(PORT, () => console.log('server ready on PORT ' + PORT))
+
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log(`server ready on PORT ${process.env.PORT || 4000}`))

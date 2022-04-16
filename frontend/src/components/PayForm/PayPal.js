f00c88ee33e1e5110959b4376eca2e60e64e7246
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-04-16 19:20:28 -0300

    Paypal basics v2

--- frontend/src/components/PayForm/PayPal.js
+++ frontend/src/components/PayForm/PayPal.js
@@ -18,20 +18,20 @@ function PayPal(props) {
     useEffect(() => {
 
         PayPalCheckOut()//LLamo al cdn de PayPal cada vez que cambia el carrito
-        // let cartItems = [];
-        // props.user.user?.basket.map((item) => {
-        //     cartItems.push({
-        //         name: item.nftId.name,
-        //         description: "The best item ever",
-        //         sku: "xyz-2654",
-        //         unit_amount: {
-        //             currency_code: "USD",
-        //             value: "100.00"
-        //         },
-        //         quantity: "1"
-        //     })
-        // })
-        // setProducts(cartItems)
+        let cartItems = [];
+        props.user.user?.basket.map((item) => {
+            cartItems.push({
+                name: item.nftId.name,
+                description: "The best item ever",
+                sku: "xyz-2654",
+                unit_amount: {
+                    currency_code: "USD",
+                    value: "100.00"
+                },
+                quantity: "1"
+            })
+        })
+        setProducts(cartItems)
 
     }, [props.user.user?.basket]);
 
@@ -48,15 +48,93 @@ function PayPal(props) {
         //Creo la orden de con los datos, esta puede ser general o con detalle de items
         console.log(data)
         return actions.order.create({
-            purchase_units: [
-                {
-                    description: "items",
-                    amount: {
-                        value: 50,
-                    },
+            // purchase_units: [
+            //     {
+            //         description: "items",
+            //         amount: {
+            //             value: 50,
+            //         },
+            //     },
+            // ], 
+
+            purchase_units: [{
+                reference_id: "PUHF",
+                description: "Sporting Goods",
+                custom_id: "CUST-HighFashions",
+                soft_descriptor: "HighFashions",
+                amount: {
+                    currency_code: "USD",
+                    value: "230.00",
+                    breakdown: {
+                        item_total: {
+                            currency_code: "USD",
+                            value: "180.00"
+                        },
+                        shipping: {
+                            currency_code: "USD",
+                            value: "30.00"
+                        },
+                        handling: {
+                            currency_code: "USD",
+                            value: "10.00"
+                        },
+                        tax_total: {
+                            currency_code: "USD",
+                            value: "20.00"
+                        },
+                        shipping_discount: {
+                            currency_code: "USD",
+                            value: "10"
+                        }
+                    }
                 },
-            ], 
-            items: products
+                items: [{
+                    name: "T-Shirt",
+                    description: "Green XL",
+                    sku: "sku01",
+                    unit_amount: {
+                         currency_code: "USD",
+                         value: "90.00"
+                    },
+                    tax: {
+                        currency_code: "USD",
+                        value: "10.00"
+                    },
+                    quantity: "1",
+                    category: "PHYSICAL_GOODS"
+                },
+                    {
+                    name: "Shoes",
+                    description: "Running, Size 10.5",
+                    sku: "sku02",
+                    unit_amount: {
+                         currency_code: "USD",
+                         value: "45.00"
+                    },
+                    tax: {
+                        currency_code: "USD",
+                        value: "5.00"
+                    },
+                    quantity: "2",
+                    category: "PHYSICAL_GOODS"
+                }
+                ],
+                shipping: {
+                    method: "United States Postal Service",
+                    address: {
+                        name: {
+                            full_name: "John",
+                            surname: "Doe"
+                        },
+                        address_line_1: "123 Townsend St",
+                        address_line_2: "Floor 6",
+                        admin_area_2: "San Francisco",
+                        admin_area_1: "CA",
+                        postal_code: "94107",
+                        country_code: "US"
+                    }
+                }
+            }]
 
         });
     };

16722320d9b5a911cb20527b72b10e4076c17abc
fernandogtorrez <fernandogtorrez@gmail.com>
2022-04-16 16:55:06 -0500

    cambios de counter

--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -5,8 +5,11 @@ import {
   Count2,
   Count3,
   Count4,
+  primerP,
+  segundoP,
 } from "../styles/CounterStyle";
 import CountUp from "react-countup";
+import '../styles/StyleCheckbox.css'
 
 const HeroCounter = () => {
   return (
@@ -20,14 +23,10 @@ const HeroCounter = () => {
             delay={0.5}
             duration={4}
             suffix="k"
-            style={{
-              fontSize: "32px",
-              fontFamily: "ubuntu",
-              fontWeight: "800",
-            }}
+            className="contador"
           />
-          <p>NFT Sold</p>
-          <p>Last Month</p>
+          <primerP>NFT Sold</primerP>
+          <segundoP>Last Month</segundoP>
         </Count1>
         <Count2>
           <CountUp
@@ -37,14 +36,10 @@ const HeroCounter = () => {
             delay={0.5}
             duration={4}
             suffix="k"
-            style={{
-              fontSize: "32px",
-              fontFamily: "ubuntu",
-              fontWeight: "800",
-            }}
+            className="contador"
           />
-          <p>New User</p>
-          <p>Last Week</p>
+          <primerP>New User</primerP>
+          <segundoP>Last Week</segundoP>
         </Count2>
         <Count3>
           <CountUp
@@ -54,14 +49,10 @@ const HeroCounter = () => {
             delay={0.5}
             duration={4}
             suffix="+"
-            style={{
-              fontSize: "32px",
-              fontFamily: "ubuntu",
-              fontWeight: "800",
-            }}
+            className="contador"
           />
-          <p>New NFT</p>
-          <p>ON Market</p>
+          <primerP>New NFT</primerP>
+          <segundoP>ON Market</segundoP>
         </Count3>
         <Count4>
           <CountUp
@@ -72,14 +63,10 @@ const HeroCounter = () => {
             delay={0.5}
             duration={4}
             suffix="ETH"
-            style={{
-              fontSize: "32px",
-              fontFamily: "ubuntu",
-              fontWeight: "800",
-            }}
+            className="contador"
           />
-          <p>Floor Price</p>
-          <p>Last Month</p>
+          <primerP>Floor Price</primerP>
+          <segundoP>Last Month</segundoP>
         </Count4>
       </Counter>
     </>
--- frontend/src/styles/CounterStyle.js
+++ frontend/src/styles/CounterStyle.js
@@ -1,167 +1,136 @@
 import styled from "@emotion/styled";
 
 export const Counter = styled.div`
-display:flex ;
-width:65% ;
-height:10rem ;
-flex-direction: row ;
-justify-items:center ;
-justify-content: space-evenly;
-margin: auto;
-margin-top: -3rem;
-@media (max-width:1000px){
-    
-    Width:100%;
-    
-    
-}
+    display:flex ;
+    flex-direction: row;
+    justify-content: space-evenly;
+    justify-items: center;
+    margin-top: -3rem;
+    @media (max-width:500px){
+        display: flex;
+        justify-content: space-between;
+    }
 `;
 
 export const Count1 = styled.div`
-display: flex;
-justify-content: space-around;
-flex-direction: column;
-Width:10rem;
-Height:8rem;  
-border-radius:13px ;
-background-color:#ffff ;
-text-align:center ;
-box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
-p:nth-child(1){
-    font-size:1.7rem ;
-    font-weight:900 ;
-}
-p:nth-child(3){
-    font-size:.8rem ;
-    font-weight:300 ;
-   
-}
-@media (max-width:1000px){
-    Width:30%;
-    min-Height:28%;
-    margin:1rem ; 
-}
-@media (max-width:500px){
-   
-   p:nth-child(1){
-   font-size:1rem ;
-   font-weight:900 ;
-}
-p:nth-child(3){
-   font-size:.8rem ;
-   font-weight:300 ;
-}
-   
-}
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    width:10rem;
+    height:8rem;  
+    border-radius:13px ;
+    background-color:#ffff ;
+    text-align:center ;
+    box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
+    /* overflow: hidden; */
+    /* padding: 8px; */
+
+    @media (max-width:1000px){
+        margin:1rem ; 
+    }
+    @media (max-width:650px){
+        padding: 6px;
+    }
+    @media (max-width:480px){
+        padding: 3.5px;
+    }
 `;
 export const Count2 = styled.div`
-display: flex;
-justify-content: space-around;
-flex-direction: column;
-Width:10rem;
-Height:8rem;  
-border-radius:13px ;
-background-color:#ffff ;
-text-align:center ;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    width:10rem;
+    height:8rem;  
+    border-radius:13px ;
+    background-color:#ffff ;
+    text-align:center ;
     box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
-p:nth-child(1){
-    font-size:1.7rem ;
-    font-weight:900 ;
-}
-p:nth-child(3){
-    font-size:.8rem ;
-    font-weight:300 ;
-}
-@media (max-width:1000px){
-    Width:30%;
-    min-Height:28%;
-    margin:1rem ;
-    
-}
-@media (max-width:500px){
-   
-   p:nth-child(1){
-   font-size:1rem ;
-   font-weight:900 ;
-}
-p:nth-child(3){
-   font-size:.8rem ;
-   font-weight:300 ;
-}
-   
-}
+
+    @media (max-width:1000px){
+        /* Width:30%;
+        min-Height:28%; */
+        margin:1rem ;
+        
+    }
+
+    @media (max-width:650px){
+        padding: 6px;
+    }
+    @media (max-width:480px){
+        padding: 3.5px;
+    }
 `;
 export const Count3 = styled.div`
-display: flex;
-justify-content: space-around;
-flex-direction: column;
-Width:10rem;
-Height:8rem; 
-border-radius:13px ;
-background-color:#ffff ;
-text-align:center ;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    width:10rem;
+    height:8rem; 
+    border-radius:13px ;
+    background-color:#ffff ;
+    text-align:center ;
     box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
-p:nth-child(1){
-    font-size:1.7rem ;
-    font-weight:900 ;
-}
-p:nth-child(3){
-    font-size:.8rem ;
-    font-weight:300 ;
-}
-@media (max-width:1000px){
-    Width:30%;
-    min-Height:28%;
-    margin:1rem ;
-    
-}
-@media (max-width:500px){
-   
-   p:nth-child(1){
-   font-size:1rem ;
-   font-weight:900 ;
-}
-p:nth-child(3){
-   font-size:.8rem ;
-   font-weight:300 ;
-}
-   
-}
+
+    @media (max-width:1000px){
+        /* Width:30%;
+        min-Height:28%; */
+        margin:1rem ;
+    }
+    @media (max-width:650px){
+        padding: 6px;
+    }
+    @media (max-width:480px){
+        padding: 3.5px;
+    }
 `;
 export const Count4 = styled.div`
-display: flex;
-justify-content: space-around;
-flex-direction: column;
-Width:10rem;
-Height:8rem;  
-border-radius:13px ; 
-background-color:#ffff ;
-text-align:center ;
-box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    width:10rem;
+    height:8rem;  
+    border-radius:13px ; 
+    background-color:#ffff ;
+    text-align:center ;
+    box-shadow: 0 12px 16px rgba(0, 0, 0, 0.270);
 
-p:nth-child(1){
+    @media (max-width:1000px){
+        /* Width:30%;
+        min-Height:28%; */
+        margin:1rem ;
+    }
+    @media (max-width:650px){
+        padding: 6px;
+    }
+    @media (max-width:480px){
+        padding: 3.5px;
+    }
+`;
+
+export const primerP = styled.div`
     font-size:1.7rem ;
     font-weight:900 ;
-}
-p:nth-child(3){
+
+    @media (max-width:500px){
+        font-size: 8px;
+        font-weight:400;
+    }
+    @media (max-width: 380px) {
+        font-size: 8px;
+        font-weight: 200;
+    }
+`;
+
+export const segundoP = styled.div`
     font-size:.8rem ;
     font-weight:300 ;
-}
-@media (max-width:1000px){
-    Width:30%;
-    min-Height:28%;
-    margin:1rem ;
-}
-@media (max-width:500px){
-   
-    p:nth-child(1){
-    font-size:1rem ;
-    font-weight:900 ;
-}
-p:nth-child(3){
-    font-size:.8rem ;
-    font-weight:300 ;
-}
-    
-}
+
+    @media (max-width:500px){
+        font-size: 8px;
+        font-weight:300 ;
+    }
+    @media (max-width: 380px) {
+        font-size: 8px;
+        font-weight: 200;
+    }
 `;
